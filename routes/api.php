7fb56dc8bf2213d5057f3f487b104524b4ac9982
Samuel Pineda Lopez <samuel.pineda@sumimedical.com>
2025-10-10 14:53:28 -0500

    feat: actualizar la zona horaria a America/Bogota y configurar la autenticación con Passport
    - Se cambió la zona horaria de la aplicación a 'America/Bogota' en config/app.php.
    
    - Se añadió un guard de autenticación para API usando Passport en config/auth.php.
    
    - Se creó el archivo de configuración passport.php para los ajustes de Passport.
    
    - Se añadieron migraciones para las tablas de OAuth: oauth_auth_codes, oauth_access_tokens, oauth_refresh_tokens, oauth_clients, oauth_device_codes.
    
    - Se actualizaron las rutas de la API para incluir middleware de autenticación y un marcador de posición para la funcionalidad de inicio de sesión.

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {
     return response()->json('pong', 200);
 });
+
+Route::middleware('auth:api')->group(function () {
+    // require __DIR__ . '/operadores/operadores.php'; 
+    return response()->json('u are authenticated', 200);
+
+});
+
+Route::get('/login', function () {
+    return response()->json('token not implemented yet');
+});

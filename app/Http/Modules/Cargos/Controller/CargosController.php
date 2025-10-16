5aa524972e105153b3d2d1f9aa7f9a3b9968d2f9
JuanPc05 <jpcastillor1@gmail.com>
2025-10-16 17:41:01 -0500

    feat: terminado el crud para cargos, se agregaron los servicios de actualizar cargo, eliminar cargo por id y mostrar cargo por id, agregados los controladores para cada funcion, y se agregaron las rutas dentro del middleware con nombres descriptivos de que hace cada ruta

--- app/Http/Modules/Cargos/Controller/CargosController.php
+++ app/Http/Modules/Cargos/Controller/CargosController.php
@@ -1,23 +1,83 @@
 <?php
 
 namespace App\Http\Modules\Cargos\Controller;
-
 use App\Http\Controllers\Controller;
 use App\Http\Modules\Cargos\Service\CargosService;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Throwable;
 
 class CargosController extends Controller
 {
     public function __construct(private CargosService $cargosService)
     {}
 
-    public function crearCargo(Request $data): \Illuminate\Http\JsonResponse
+    public function crearCargo(Request $request): JsonResponse
     {
         try {
-            $cargo = $this->cargosService->crearCargo($data->all());
+
+            $validated = $request->validate([
+                'nombre' => 'required|string|max:255',
+            ]);
+
+            $cargo = $this->cargosService->crearCargo($validated);
             return response()->json($cargo, 201);
+        } catch (Throwable $th) {
+            return response()->json(['error' => 'Error al crear cargo'], 500);
+        }
+    }
+
+    public function listarCargos(): JsonResponse
+    {
+        try{
+            $cargos = $this->cargosService->listarCargos();
+            return response()->json($cargos, 200);
         } catch (\Throwable $th) {
-            return response()->json(['error' => 'An error occurred'], 500);
+            return response()->json(['error' => 'Error al listar los cargos'], 500);
+        }
+    }
+
+    public function mostrarCargoPorId($id): JsonResponse
+    {
+        try {
+            $cargo = $this->cargosService->mostrarCargoPorId($id);
+
+            return response()->json($cargo, 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Cargo no encontrado'], 404);
+        } catch (Throwable $th) {
+            return response()->json(['error' => 'Error al mostrar el cargo'], 500);
+        }
+    }
+
+    public function actualizarCargo(Request $request, $id): JsonResponse
+    {
+        try {
+            $validated = $request->validate([
+                'nombre' => 'required|String|max:255',
+            ]);
+            $cargo = $this->cargosService->actualizarCargo($id, $validated);
+
+            return response()->json($cargo, 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Cargo no encontrado'], 404);
+        } catch (Throwable $th) {
+            return response()->json(['error' => 'Error al actualizar el cargo'], 500);
+        }
+    }
+
+    public function eliminarCargoPorId($id): JsonResponse
+    {
+        try {
+            $this->cargosService->eliminarCargoPorId($id);
+            return response()->json([
+                'mensaje' => 'Cargo eliminado correctamente',
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Cargo no encontrado'], 404);
+        } catch (Throwable $th) {
+            return response()->json(['error' => 'Error al eliminar el cargo'], 500);
         }
     }
 }
